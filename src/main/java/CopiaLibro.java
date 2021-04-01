56ed0f396f9e2ff97fdce7ecc37fdc8eed3d3018
Lucas Guardiola <LJGuardiola@gmail.com>
2021-03-31 22:12:11 -0300

    Commit inicial

---
+++ src/main/java/CopiaLibro.java
@@ -0,0 +1,9 @@
+public class CopiaLibro {
+    private Libro libro;
+    public CopiaLibro(Libro libro) {
+        this.libro = libro;
+    }
+    public Libro libro() {
+        return libro;
+    }
+}
